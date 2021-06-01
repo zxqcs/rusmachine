5e85efd7bfb10c53e487ec44e27aff814146d6e2
yccsml <yccsml@163.com>
2021-06-01 16:09:24 +0800

    implement type system for Lisp, part 1

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use infrastructure::{register::Item, stack::Stack};
 
 mod infrastructure;
 mod basic_machine;
+mod representation;
 fn main() {
     let mut s = Stack::new();
     s.push(Item::Number(1.0));
